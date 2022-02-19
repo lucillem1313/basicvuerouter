d9922ed93feaf0bc96bbc1f76103b9fce3d3a661
lucillem1313 <https://github.com/lucillem1313>
2022-02-18 20:33:33 -0600

    for route

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -8,7 +8,7 @@
 <nav>
 <ul class="space-x-6"><router-link to="/Home">
 
-<li class="bg-gray-400 text-lg py-3 px-4 hover:cursor-pointer text-center hover:bg-purple-500 hover:text-orange-500"> Home </li></router-link> 
+<li class="bg-gray-400 text-lg py-4 px-4 hover:cursor-pointer text-center hover:bg-purple-500 hover:text-orange-500"> Home </li></router-link> 
 <router-link to="/About"><li class="bg-gray-400 tx-lg py-3 px-4 hover:cursor-pointer text-center hover:bg-blue-500 hover:text-orange-500"> About</li></router-link> 
 <router-link to="/Services"><li class="bg-gray-400 tx-lg py-3 px-4 hover:cursor-pointer text-center hover:bg-pink-500 hover:text-red-500"> Services</li></router-link> 
 <router-link to="/Help"><li class="bg-gray-400 tx-lg py-3 px-4 hover:cursor-pointer text-center hover:bg-orange-500 h6over:text-red-500">Help</li></router-link> 
